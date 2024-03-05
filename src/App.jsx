fffe2935780bf893bcf651c5ebe4add18f63eeed
Ibbad-Shabbir <ibbadshabbir02@gmail.com>
2024-03-05 19:10:12 +0500

    created a resuable button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 
-function App() {
-  return <div>WOW</div>;
-}
+const App = () => {
+  return (
+    <>
+      <div>WOW</div>
+
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          something
+        </Button>
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
