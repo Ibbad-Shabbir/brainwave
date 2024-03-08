86d5527d98cbc41ccc38ae714abed50fa160ea4a
Ibbad-Shabbir <ibbadshabbir02@gmail.com>
2024-03-08 17:00:04 +0500

    Navigation done *Mobile and Desktop both*

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
@@ -8,9 +7,7 @@ const App = () => {
       <div>WOW</div>
 
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          something
-        </Button>
+        <Header />
       </div>
 
       <ButtonGradient />
